160892f3f054743880ece5295100a239513b4818
Charles <cdcomey@ucdavis.edu>
2023-05-14 22:26:47 -0700

    added arg parsing

--- sshell.c
+++ sshell.c
@@ -17,6 +17,42 @@ struct Command{
     char** args;
 };
 
+struct Command* initCommand(void){
+    struct Command* command = malloc(sizeof(struct Command));
+    command->main_command[0] = '\0';
+    command->num_args = 0;
+    command->args = malloc(MAX_ARGS * sizeof(char*));
+
+    for (unsigned i = 0; i < MAX_ARGS; i++){
+        command->args[i] = malloc(TOKEN_MAX * sizeof(char));
+        memset(command->args[i], '*', TOKEN_MAX * sizeof(char));
+    }
+
+    return command;
+}
+
+bool destroyCommand(struct Command* command){
+    if (command == NULL){
+        return false;
+    }
+
+    for (unsigned i = 0; i < MAX_ARGS; i++){
+        free(command->args[i]);
+    }
+
+
+    free(command->args);
+    free(command);
+    return true;
+}
+
+void printCommandStruct(struct Command* cmd){
+    printf("main command: %s\n", cmd->main_command);
+    for (unsigned i = 0; i < cmd->num_args; i++){
+        printf("arg%d: %s\n", i, cmd->args[i]);
+    }
+}
+
 /**
  * removes leading and trailing whitespace
  * removes leading whitespace by incrementing the pointer until it reaches a non-space
@@ -63,6 +99,49 @@ bool parseOutputRedirection(const char* cmd_line, char* core_command, char* outp
     return true;
 }
 
+bool parseArgs(const char* cmd_line, struct Command* command){
+
+
+    char cmd_copy_array[sizeof(cmd_line)];
+    strcpy(cmd_copy_array, cmd_line);
+    char* cmd_copy = &cmd_copy_array[0];
+    printf("set up cmd_copy\n");
+
+    // command is the first word in cmd
+    // ex: in $ ls -l, ls is the main_command
+    strcpy(command->main_command, strtok(cmd_copy, " "));
+    // printf("main command = %s\n", command->main_command);
+
+    // the first arg is the command itself
+    strcpy(command->args[0], command->main_command);
+    (command->num_args) = 1;
+
+    // for every other arg, add every word in the rest of cmd as an element of args
+    for (unsigned i = 1; ; i++){
+            char* tok = strtok(NULL, " ");
+            // printf("tokenized arg\n");
+
+            if (tok != NULL && command->num_args == MAX_ARGS){
+                    fprintf(stderr, "too many process arguments\n");
+                    exit(-1);
+            }
+
+            // the last arg should always be null
+            // so if it is, break
+            if (tok == NULL){
+                    // strcpy doesn't work with NULL, so it must be set directly
+                    command->args[i] = NULL;
+                    (command->num_args)++;
+                    break;
+            } else {
+                    strcpy(command->args[i], tok);
+                    (command->num_args)++;
+            }
+    }
+
+    return command->num_args > 1;
+}
+
 
 int main(){
 
@@ -96,6 +175,10 @@ int main(){
 
         parseOutputRedirection(cmd, core_command, output_file);
         printf("core command: %s\noutput file: %s\n", core_command, output_file);
+
+        struct Command* cmd1 = initCommand();
+
+        destroyCommand(cmd1);
     }
 
     return 0;
